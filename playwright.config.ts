99e22cd3f446de40429ccbdbf29a4d134d04be12
Lavinia Micu <laviniamicu78@gmail.com>
2025-09-25 13:38:32 +0200

    update playwright.config.ts adding reporters so Azure can publish reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'https://dummyjson.com',
-    extraHTTPHeaders: {
-      'Content-Type': 'application/json'
-    }
-  }
+    extraHTTPHeaders: {'Content-Type': 'application/json'},
+  },
+  reporter: [
+      ['list'],
+      ['junit', { outputFile: 'test-results/junit.xml' }],
+      ['html', { outputFolder:'playwright-report',open: 'never' }],
+    ],
+  
 });
